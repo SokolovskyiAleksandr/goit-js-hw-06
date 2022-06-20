f6a73f4d4b8663e0bc27bd1bc88a8da0f49a0693
Sokolovskyi Aleksandr <a.sokol19892104@gmail.com>
2022-06-20 14:32:53 +0300

    9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const bgcBody = document.querySelector('body');
+const colorValue = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  bgcBody.style.backgroundColor = color;
+  colorValue.textContent = color;
+});
