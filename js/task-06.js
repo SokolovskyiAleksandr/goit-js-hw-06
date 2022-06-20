6866b87a0f68b5d15be0749db1cd3bbc7bb233ea
Sokolovskyi Aleksandr <a.sokol19892104@gmail.com>
2022-06-20 13:22:13 +0300

    6

---
+++ js/task-06.js
@@ -0,0 +1,22 @@
+const textInput = document.querySelector("#validation-input");
+const lengthData = document.querySelector('input[data-length]');
+const inputLength = lengthData.dataset.length;
+
+textInput.addEventListener('blur', (text) => {
+
+    if (text.currentTarget.value.length < inputLength || text.currentTarget.value.length > inputLength) {
+        textInput.classList.remove('valid');
+        textInput.classList.add('invalid');
+    }
+    else {
+        textInput.classList.remove('invalid');
+        textInput.classList.add('valid');
+                console.log(
+            'else', text.currentTarget.value.length
+        )
+                console.log(
+            'else', inputLength
+        )
+    }
+}
+)
