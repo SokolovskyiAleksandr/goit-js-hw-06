93ef6977271bf80cf92a594d11c06f7b7f2836b4
Sokolovskyi Aleksandr <a.sokol19892104@gmail.com>
2022-06-20 14:09:06 +0300

    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+event.preventDefault();
+const {
+    elements: { email, password },
+} = event.currentTarget;
+
+    if (email.value === '' && password.value === '') {
+        return alert('Please fill in all fields!');
+    }
+    else
+        if (email.value === '') {
+            return alert('please add email!');
+        } else
+            if (password.value === '') {
+                return alert('Please add password!');
+            }
+
+const valueObj = {
+    email: email.value,
+    password: password.value,
+};
+
+console.log(valueObj);
+event.currentTarget.reset();
+}
